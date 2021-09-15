3eb05205d8c4b7255709ce168968385101dff7cd
Hasan Hüseyin GÜNEŞ <71901498+guneshhg@users.noreply.github.com>
2021-09-15 17:12:02 +0300

    RateLimit
    RateLimit

---
+++ RateLimit_API/RateLimit_API/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RateLimit_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCategory()
+        {
+            return Ok(new { Id = 1, Category = "Car" });
+        }
+        
+        [HttpPost]
+        public IActionResult SaveProduct()
+        {
+            return Ok(new { Status = "Success" });
+        }
+
+
+    }
+}
---
+++ RateLimit_API/RateLimit_API/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RateLimit_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok(new { Id = 1, Name = "Kalem", Price = 20 });
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult>UpdateProduct()
+        {
+            return Ok();
+        }
+
+        // api/product/kalem/20
+        [HttpGet("{name}/{price}")]
+        public IActionResult GetProduct(string name,int price)
+        {
+            return Ok(name);
+        }
+
+
+
+
+
+
+    }
+}
